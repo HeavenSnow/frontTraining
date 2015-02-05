a75bb3cee1027d6d642bce8c95301dc02366ad75
lily.qi <lily.qi@trantect.com>
2015-02-05 15:46:06 +0800

    Complete the BasicWebProject

---
+++ BasicWebProject/result/result.js
@@ -0,0 +1,14 @@
+/**
+ * Created by developer on 2/5/15.
+ */
+
+var onClickImages = function(){
+  document.getElementById("btn").style.color = "red";
+  console.log("click image");
+}
+
+var onClickEnter = function () {
+  window.confirm("Welcome");
+  console.log("click Enter");
+}
+
