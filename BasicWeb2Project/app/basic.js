a8512a90113b459e998d1c7147052a385604de8d
lily.qi <lily.qi@trantect.com>
2015-02-11 12:24:35 +0800

    Add the app directory in BasicWeb2Project success

---
+++ BasicWeb2Project/app/basic.js
@@ -0,0 +1,24 @@
+/**
+ * Created by developer on 2/5/15.
+ */
+
+var onClickImages = function(){
+  document.getElementById("btn").style.color = "red";
+  console.log("click image");
+}
+
+/* TODO: 在第 23 行完成
+  1. 使用jquery,定义id为"account"的submit方法；
+  2. 在submit方法中实现：
+    2.1 当id为 username 的输入框内容为"super";
+    2.2 并且当id为 password 的输入框内容为"super123";
+    2.3 并且当id为 code 的输入框内容为"2273";
+    2.4 同时满足以上三个要求时弹出一个confirm确认框，确认的内容为
+    　　“Congratulation! You have logged Success！"
+    2.5 若不满足任何一个条件：
+    　* 找到所有的"form-login"样式的输入框;
+    　* 给这些输入框添加一个错误的样式"has-error";
+    　* 接着弹出一个alert提醒框，提醒内容为"Login Failed!"
+*/
+
+
